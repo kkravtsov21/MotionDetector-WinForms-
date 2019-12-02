5cb67aace5ed607cd3a708c557668a3a63b82932
kkravtsov21 <45846731+kkravtsov21@users.noreply.github.com>
2019-12-02 17:49:06 +0200

    Add files via upload

---
+++ MotionDetection/MotionDetection/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace MotionDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SaveMode = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SoundMode = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(35, 70);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(611, 425);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            this.videoSourcePlayer1.Click += new System.EventHandler(this.videoSourcePlayer1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(35, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(242, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID Устройства";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(283, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(384, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(522, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во точек движения";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(525, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SaveMode
+            // 
+            this.SaveMode.AutoSize = true;
+            this.SaveMode.Location = new System.Drawing.Point(807, 12);
+            this.SaveMode.Name = "SaveMode";
+            this.SaveMode.Size = new System.Drawing.Size(154, 21);
+            this.SaveMode.TabIndex = 7;
+            this.SaveMode.Text = "Режим сохранения";
+            this.SaveMode.UseVisualStyleBackColor = true;
+            this.SaveMode.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(652, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(431, 425);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SoundMode
+            // 
+            this.SoundMode.AutoSize = true;
+            this.SoundMode.Location = new System.Drawing.Point(807, 37);
+            this.SoundMode.Name = "SoundMode";
+            this.SoundMode.Size = new System.Drawing.Size(137, 21);
+            this.SoundMode.TabIndex = 9;
+            this.SoundMode.Text = "Звуковой режим";
+            this.SoundMode.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 507);
+            this.Controls.Add(this.SoundMode);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.SaveMode);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox SaveMode;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox SoundMode;
+    }
+}
+
---
+++ MotionDetection/MotionDetection/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+
+
+namespace MotionDetection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection Devices;
+        private VideoCaptureDevice VidSource;
+        public Bitmap GetCurrentVideoFrame;
+
+        MotionDetector Detector;
+        float DetLevel;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer1.SignalToStop();            
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (VidSource.IsRunning == true)
+            {
+                VidSource.Stop();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Detector = new MotionDetector(new TwoFramesDifferenceDetector(), new MotionBorderHighlighting());
+            DetLevel = 0;
+
+            Devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo x in Devices)
+            {
+                comboBox1.Items.Add(x.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VidSource = new VideoCaptureDevice(Devices[comboBox1.SelectedIndex].MonikerString);
+            videoSourcePlayer1.VideoSource = VidSource;
+            videoSourcePlayer1.Start();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void videoSourcePlayer1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            DetLevel = Detector.ProcessFrame(image);           
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Format("{0:00.0000}", DetLevel);
+            if (SaveMode.Checked == true && DetLevel >=00.0500)
+            {
+                
+                    Bitmap bitmap = (Bitmap)videoSourcePlayer1.GetCurrentVideoFrame().Clone();
+                    pictureBox1.Image = bitmap;
+                    pictureBox1.Image.Save(@"C:\\Detector\MDTest.bmp", ImageFormat.Bmp);                
+            }
+            else if (SoundMode.Checked == true && DetLevel>=00.0500)
+            {
+                Console.Beep();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
